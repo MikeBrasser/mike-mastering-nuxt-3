67b020d4b686e3aa1cb7253b447c169bca93a541
Mike Brasser <mike@arlanet.com>
2023-02-22 11:46:39 +0100

    Done: Till chapter 7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
+import vsharp from 'vite-plugin-vsharp';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    stripeSecret: '',
+    public: {
+      stripeKey: '',
+    },
+  },
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
+  vite: {
+    plugins: [vsharp()],
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@nuxtjs/supabase',
+    '@pinia/nuxt',
   ],
 });
--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -9,17 +9,27 @@ const lessonSelect = Prisma.validator<Prisma.LessonArgs>()({
     number: true,
   },
 });
-export type LessonOutline = Prisma.LessonGetPayload<typeof lessonSelect>;
+export type LessonOutline = Prisma.LessonGetPayload<
+  typeof lessonSelect
+> & {
+  path: string;
+};
 
-const chapterSelect = Prisma.validator<Prisma.ChapterArgs>()({
-  select: {
-    title: true,
-    slug: true,
-    number: true,
-    lessons: lessonSelect,
-  },
-});
-export type ChapterOutline = Prisma.ChapterGetPayload<typeof chapterSelect>;
+const chapterSelect =
+  Prisma.validator<Prisma.ChapterArgs>()({
+    select: {
+      title: true,
+      slug: true,
+      number: true,
+      lessons: lessonSelect,
+    },
+  });
+export type ChapterOutline = Omit<
+  Prisma.ChapterGetPayload<typeof chapterSelect>,
+  'lessons'
+> & {
+  lessons: LessonOutline[];
+};
 
 const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
   select: {
@@ -27,8 +37,39 @@ const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
     chapters: chapterSelect,
   },
 });
-export type CourseOutline = Prisma.CourseGetPayload<typeof courseSelect>;
+export type CourseOutline = Omit<
+  Prisma.CourseGetPayload<typeof courseSelect>,
+  'chapters'
+> & {
+  chapters: ChapterOutline[];
+};
 
-export default defineEventHandler(() => {
-  return prisma.course.findFirst(courseSelect);
-});
+export default defineEventHandler(
+  async (): Promise<CourseOutline> => {
+    const outline = await prisma.course.findFirst(
+      courseSelect
+    );
+
+    // Error if there is no course
+    if (!outline) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: 'Course not found',
+      });
+    }
+
+    // Map the outline so we can add a path to each lesson
+    const chapters = outline.chapters.map((chapter) => ({
+      ...chapter,
+      lessons: chapter.lessons.map((lesson) => ({
+        ...lesson,
+        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+      })),
+    }));
+
+    return {
+      ...outline,
+      chapters,
+    };
+  }
+);
